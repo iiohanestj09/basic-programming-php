0eeb24410cfcd256504da9cd50f2a4ef6d75da9c
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-08 16:51:49 +0700

    Menambah poin Magic constant di materi constant

--- a_awal/4_constant.php
+++ a_awal/4_constant.php
@@ -16,3 +16,13 @@ echo NAMA . "\n";
 echo PI . "\n";
 echo JENISKEL . "\n";
 echo APP_VERSION . "\n";
+
+
+// Magic Constant  -> Nilai otomatis dari PHP
+echo __LINE__ . "\n";       // Nomor baris skrip saat ini
+echo __FILE__ . "\n";       // Path lengkap file saat ini    
+echo __DIR__ . "\n";        // Direktori file saat ini
+echo __FUNCTION__ . "\n";   // Nama fungsi saat ini
+echo __CLASS__ . "\n";      // Nama class saat ini
+echo __METHOD__ . "\n";     // Nama method saat ini
+echo __NAMESPACE__ . "\n";  // Namespace saat ini
