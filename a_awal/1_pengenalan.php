53d4d27d3f9f64bd53666f3d80ff4c4df73ec709
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-08 15:30:50 +0700

    First commit

---
+++ a_awal/1_pengenalan.php
@@ -0,0 +1,14 @@
+<?php
+// PHP adalah kepanjangan dari 'PHP: Hypertext Preprocessor'
+
+/* Perintah untuk menampilkan output biasanya: 
+- echo : bisa memisahkan banyak argumen dengan koma
+- print : hanya bisa menerima satu argumen, tetapi dapat digunakan dalam ekspresi
+*/ 
+
+echo "Hello World\n";
+print "Hello World!!\n";
+
+echo "Halo Dunia", ", ini dengan Putra\n";
+
+// Untuk mengeksekusi file .php di terminal, pakai 'php namaFile.php'
