a2f92a89102abacf70114b5c0a138d87420d74b3
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-08 15:44:07 +0700

    Menambah materi struktur_file

---
+++ a_awal/2_struktur_file.php
@@ -0,0 +1,23 @@
+<?php
+//  ini komentar di PHP
+/* ini juga
+ini juga
+*/
+
+// Menulis kode PHP diawali dengan tag <?php.
+// Untuk file yang sepenuhnya PHP, tag penutup tidak perlu
+// Untuk file yang TIDAK sepenuhnya PHP, harus diberi tag penutup agar ada pemisahan kode antara PHP dan yang bukan
+
+echo "Holaaa\n";
+?>
+
+<!-- ini komentar di HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>2_struktur_file</title>
+</head>
+<body>
+    <?php echo "Halooo juga\n"; ?>
+</body>
+</html>
