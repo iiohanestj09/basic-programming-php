36c568daaea668f12c7b9f17a38ad0a0bdebb69b
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-08 18:22:41 +0700

    Menambah materi tipe_data_string

---
+++ a_awal/6_tipe_data_string.php
@@ -0,0 +1,48 @@
+<?php
+// String adalah tipe data yang berisi teks yang diapit oleh tanda '' ataupun ""
+
+// 1. Membuat String
+$teks1 = 'Halo Dunia!';
+$teks2 = "Halo Dunia!!";
+echo $teks1. "\n";
+echo $teks2. "\n";
+
+
+// 2. Karakter Escape
+echo "Halo\thah\n";
+echo "Dia berkata: \"Hahaha\"\n";
+echo "D:VS CODE\\6_PHP\\a_awal\\\n";
+
+
+// 3. Menggabungkan string menggunakan operator titik (.)
+$nama = "Putra";
+$namaLengkap = "Yohanes". $nama. " Dae";
+echo $namaLengkap. "\n";
+
+
+// 4. String Multi-Baris
+$pesan = <<<TEXT
+haloo, nama saya $nama.             holaa
+Saya sedang belajar PHP Dasar.\n
+TEXT;
+
+echo $pesan;
+
+
+/* 5. Fungsi-Fungsi Penting
+- strlen($str)                      : Menghitung panjang string
+- str_word_count($str)              : Menghitung jumlah karakter
+- strtoupper($str)                  : Ubah ke upperCase
+- strtolower($str)                  : Ubah ke lowerCase
+- ucfirst($str)                     : Huruf pertama jadi kapital
+- substr($str, start, length)       : Mengambil potongan string
+- str_replace($cari, $ganti, $str)  : Menganti teks
+- str_contains($str, kalimat)       : Apakah kalimat berada di string
+- str_starts_with($str, kalimat)    : apakah kalimat berada di awal string
+- str_ends_with($str, kalimat)      : Apakah kalimat berada di akhir string
+*/
+
+$teks3 = "Latihan Javascript";
+
+echo str_replace("Latihan", "Belajar", $teks3). "\n";
+echo str_replace("Javascript", "PHP", $teks3). "\n";
