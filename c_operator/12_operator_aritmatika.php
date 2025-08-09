dd9bd25ac0f0841eac97721ad6627d7583515af0
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-09 12:14:24 +0700

    Menambah materi operator_aritmatika

---
+++ c_operator/12_operator_aritmatika.php
@@ -0,0 +1,10 @@
+<?php
+// Tambah (+), Kurang (-), Kali (*), Bagi(/) 
+// Modulus (%)
+// Pangkat (**)
+
+$a = 3;
+$b = 4;
+$c = 0.5;
+$d = 1.7;
+var_dump($a + $b, $a - $b, $b * $d, $a / $c, $a % $b, $b ** $a);
