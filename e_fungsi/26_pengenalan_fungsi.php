dbb064a6beb9c52c38839b5465cfb159599bae20
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-09 19:04:23 +0700

    Menambah materi pengenalan_function

---
+++ e_fungsi/26_pengenalan_fungsi.php
@@ -0,0 +1,44 @@
+<?php
+// 1. membuat fungsi
+function salam() {
+    echo "Hallooo Duniaa\n";
+}
+salam();
+
+
+// 2. Function dengan parameter
+function sapa($nama) {
+    echo "Hallo ". $nama. "\n";
+}
+sapa("Budi");
+
+
+// 3. Function dengan default parameter
+function panggil($nama = "Brooo") {
+    echo "Haii ". $nama. "\n";
+}
+panggil();
+
+
+// 4. Function dengan return value
+function tambah($a, $b) {
+    return $a + $b;
+}
+echo tambah(2, 3). "\n";
+
+
+// 5. Function dengan type declaration
+function kali(int $a, int $b) {
+    return $a * $b;
+}
+// echo kali(2.2, 3). "\n";         Deprecated: semacam warning bahwa (float) 2.2 akan diubah jadi (int) 2 di fungsi
+echo kali(3, 3). "\n";
+
+
+// 6. Variabel Scope
+$globalScope = 10;
+function coba() {
+    global $globalScope;
+    echo $globalScope. "\n";
+}
+coba();
