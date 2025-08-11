9c1897b996f42f8cd5ff00fe9706068eb044d2b5
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-11 16:47:43 +0700

    Menambah materi iterasi_array

---
+++ f_array/39_iterasi_array.php
@@ -0,0 +1,55 @@
+<?php
+$data1 = ["Apel", "Jeruk", "Manggis"];
+$data2 = [
+    'nama' => 'Budi',
+    'umur' => 20,
+    'aktif' => true
+];
+
+// 1. Menggunakan foreach
+  // Indexed Array
+foreach ($data1 as $item) {
+    echo "$item ";
+}
+echo PHP_EOL;
+
+foreach ($data1 as $index => $item) {       // dengan index
+    echo "$index: $item ";
+}
+echo PHP_EOL;
+
+  // Associative Array
+foreach ($data2 as $item) {     // Iterasi value saja
+    echo "$item ";
+}
+echo PHP_EOL;
+
+foreach(array_keys($data2) as $key) {       // Iterasi key saja  
+    echo "$key ";
+}
+echo PHP_EOL;
+
+foreach ($data2 as $key => $value) {        // Iterasi Lengkap
+    echo "$key: $value ";
+}
+echo PHP_EOL;
+
+
+// 2. Menggunakan for -> (Indexed Array)
+for ($i = 0; $i < count($data1); $i++) {
+    echo "$data1[$i] ";
+}
+echo PHP_EOL;
+
+
+// 3. Menggunakan while + current() + next() -> (Indexed Array)
+while ($item = current($data1)) {
+    echo "$item ";
+    next($data1);
+}
+
+
+// 4. Membuat array baru dengan iterasi
+$angka = [1, 2, 3];
+$kuadrat = array_map(fn($n) => $n * $n, $angka);
+print_r($kuadrat);
