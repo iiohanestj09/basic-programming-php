fb15c68ba03e4ca8ab382d88dedf8b41f84c30e7
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-11 15:28:42 +0700

    Update note di manipulasi_indexed_array

--- f_array/37_manipulasi_indexed_array.php
+++ f_array/37_manipulasi_indexed_array.php
@@ -1,8 +1,20 @@
 <?php
-// Manipulasi Jenis Indexed Array
+/* Manipulasi Jenis Indexed Array
+1. Menambah elemen
+2. Menghapus elemen
+3. Mengubah Nilai elemen
+4. Menggabungkan Array
+5. Memotong Array
+6. Replace Sebagian
+7. Reverse
+8. Sorting
+9. Searching
+10. Counting
+*/
+
 $buah = ["Apel", true, 45, 33.3];
 
-// 1. Menambah Elemen
+//! 1. Menambah Elemen
   // A. Dengan []
 $buah[] = "Mangga";     // "Mangga" akan ditambahkan index paling belakang
 
@@ -11,7 +23,7 @@ array_push($buah, false, "Pisang");
 print_r($buah);
 
 
-// 2. Menghapus Elemen
+//! 2. Menghapus Elemen
 unset($buah[3]);
 print_r($buah);
 //? NOTES: Index tidak otomatis berurutan kembali. Saat array berisi index 0 1 2 3 4, lalu unset index 3, maka 0 1 2 4
@@ -21,11 +33,11 @@ $buah = array_values($buah);
 print_r($buah);
 
 
-// 3. Mengubah Nilai Elemen
+//! 3. Mengubah Nilai Elemen
 $buah[0] = 6.63333;
 
 
-// 4. Menggabungkan Array
+//! 4. Menggabungkan Array
 $a = [1, 2];
 $b = [9, 1, 7];
 $c = ["Ayam", "Babi", "Tikus"];
@@ -35,13 +47,13 @@ print_r($d);
 print_r($e);
 
 
-// 5. Memotong Array  -> array_slice()
+//! 5. Memotong Array  -> array_slice()
 $data1 = ['a', 'b', 3, true, null, 'f', 'g'];
 $potong = array_slice($data1, 1, 4);     // array_slice($array, start, jumlah elemen ke kanan)
 print_r($potong);
 
 
-// 6. Menghapus dan Mengganti  -> array_splice() {Mengubah Array Asli}
+//! 6. Menghapus dan Mengganti  -> array_splice() {Mengubah Array Asli}
 $data2 = ['Apel', 'Jeruk', 'Mangga', 'Pisang'];
 array_splice($data2, 2, 1, ["Anggur", "Apalah"]);  // array_splice($array, start, jumlah elemen ke kanan yg dihapus, [elemen baru]);
 // Jika jumlah elemen ke kanan yg dihapus lebih kecil dari jumlah elemen yhg ditambah, maka yg ditambahkan akan diselipkan dari index tersebut
@@ -52,19 +64,19 @@ array_splice($data2, 1, 1);
 print_r($data2);
 
 
-// 7. Membalik Urutan Array
+//! 7. Membalik Urutan Array
 $coba = [1, 2, 3, 4, 5];
 print_r(array_reverse($coba));      // 5 4 3 2 1
 
 
-// 8. Mengurutkan Array
+//! 8. Mengurutkan Array
 sort($coba);    // Ascending (Kecil -> Besar)   1 2 3 4 5
 asort($coba);   // Descending (Besar -> kecil)  5 4 3 2 1
 
 
-// 9. Mencari elemen
+//! 9. Mencari elemen
 if (in_array(3, $coba)) echo "Ada angka 3\n";
 
 
-// 10. Menghitung jumlah elemen
+//! 10. Menghitung jumlah elemen
 echo count($coba) . PHP_EOL;
