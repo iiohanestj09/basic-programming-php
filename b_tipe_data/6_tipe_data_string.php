5306e93eb626b5664d3617d1d2f0ba2c1ad95dec
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-12 22:45:18 +0700

    Update materi tipe_data_string

--- b_tipe_data/6_tipe_data_string.php
+++ b_tipe_data/6_tipe_data_string.php
@@ -27,24 +27,3 @@ Saya sedang belajar PHP Dasar.\n
 TEXT;
 
 echo $pesan;
-
-
-/* 5. Fungsi-Fungsi Penting
-- strlen($str)                      : Menghitung panjang string
-- str_word_count($str)              : Menghitung jumlah kata (dipisah oleh spasi)
-- strtoupper($str)                  : Ubah ke upperCase
-- strtolower($str)                  : Ubah ke lowerCase
-- ucfirst($str)                     : Huruf pertama jadi kapital
-- substr($str, start, length)       : Mengambil potongan string
-- str_replace($cari, $ganti, $str)  : Menganti teks
-- str_contains($str, kalimat)       : Apakah kalimat berada di string
-- str_starts_with($str, kalimat)    : apakah kalimat berada di awal string
-- str_ends_with($str, kalimat)      : Apakah kalimat berada di akhir string
-*/
-
-$teks3 = "halo123 yahaha";
-echo strlen($teks3). " ". str_word_count($teks3). "\n";
-
-$teks4 = "Latihan Javascript";
-echo str_replace("Latihan", "Belajar", $teks4). "\n";
-echo str_replace("Javascript", "PHP", $teks4). "\n";
