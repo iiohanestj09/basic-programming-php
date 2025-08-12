90a3ed991b388dfa993f79b71bc002210e0d01c5
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-13 00:06:26 +0700

    Update materi fwrite di tulis_file

--- g_file/44_tulis_file/fwrite.php
+++ g_file/44_tulis_file/fwrite.php
@@ -1,7 +1,14 @@
 <?php
+// 2. fwrite()
 // Fungsi ini menulis data ke file langsung sekaligus
 // Jika file belum ada -> akan dibuat
-// Jika file sudah ada -> isinya akan ditimpa (overwrite), kecuali pakai flag tertentu
+// Jika file sudah ada -> dengan fwrite() tidak akan overwrite
+
+/*
++ Fleksibel utk proses tulis yg kompleks / file besar
++ Bisa mengatur posisi tulis (pakai fseek())
+- Lebih banyak kode dibanding file_put_contents()
+*/
 
 $handle = fopen("text2.txt", "w");
 
